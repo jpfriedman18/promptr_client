59da7ffb77f2f8ea9cfef441166906896b4ef4ba
jpfriedman18 <jpfriedman18@gmail.com>
2016-04-05 14:14:07 -0400

    Redirect to teacher-landing after prompt creation

--- app/create-prompt/route.js
+++ app/create-prompt/route.js
@@ -4,7 +4,8 @@ export default Ember.Route.extend({
   actions: {
     createPrompt: function(properties){
       this.store.createRecord('prompt', properties)
-        .save().then(()=>console.log('prompt created'));
+        .save().then(()=>console.log('prompt created'))
+        .then(()=> this.transitionTo('teacher-landing'));
     },
   }
 });
