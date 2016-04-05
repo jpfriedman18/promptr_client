62a07be6603ae6567f0d7241943caaaa8be7daac
jpfriedman18 <jpfriedman18@gmail.com>
2016-04-05 14:48:29 -0400

    Clean up teacher-landing route file

--- app/teacher-landing/route.js
+++ app/teacher-landing/route.js
@@ -1,19 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(){
-    return {
-      prompt: this.store.findAll('prompt'),
-    };
-  },
-  actions: {
-    createPrompt: function(properties){
-      this.store.createRecord('prompt', properties)
-        .save().then(()=>console.log('prompt created'));
-    },
-    destroyPrompt: function(prompt){
-      prompt.destroyRecord()
-      .then(()=>console.log("prompt destroyed"));
-    }
-  }
+
 });
