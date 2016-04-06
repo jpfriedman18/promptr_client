1f923b541984a180d1abc3e107582620666e6cbf
jpfriedman18 <jpfriedman18@gmail.com>
2016-04-06 19:38:22 -0400

    Add prompt info to evaluation snippet

--- app/student-evaluations/route.js
+++ app/student-evaluations/route.js
@@ -4,12 +4,6 @@ export default Ember.Route.extend({
   model: function(){
     return {
       evaluation: this.get('store').findAll('evaluation'),
-      // promptResponse: this.get('store').findAll('prompt_response'),
     };
   },
-  actions:{
-    logEvaluations: function(){
-      console.log(this.get('model.evaluation'));
-    },
-  }
 });
