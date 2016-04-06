edef38b07d669591c1b87df9faf14993a2611fdb
jpfriedman18 <jpfriedman18@gmail.com>
2016-04-06 18:25:03 -0400

    Create student-evaluations route and link to student landing

--- app/router.js
+++ app/router.js
@@ -15,6 +15,7 @@ Router.map(function () {
   this.route('create-prompt');
   this.route('prompts');
   this.route('student-prompt-responses');
+  this.route('student-evaluations');
 });
 
 export default Router;
