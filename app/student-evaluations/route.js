520dfcc7acfafeedac22a0b2be23ef5aaf474c52
jpfriedman18 <jpfriedman18@gmail.com>
2016-04-07 16:24:57 -0400

    Rename evaluation to evaluations

--- app/student-evaluations/route.js
+++ app/student-evaluations/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(){
     return {
-      evaluation: this.get('store').findAll('evaluation'),
+      evaluations: this.get('store').findAll('evaluation'),
     };
   },
 });
