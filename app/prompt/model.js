f12940f9e4bd209964a7533dd4585cfa0b461643
jpfriedman18 <jpfriedman18@gmail.com>
2016-04-07 10:13:42 -0400

    Show teacher name on prompt for students

--- app/prompt/model.js
+++ app/prompt/model.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   text: DS.attr('string'),
-  promptResponses: DS.hasMany('prompt-response', {async: true})
+  promptResponses: DS.hasMany('prompt-response', {async: true}),
+  teacher: DS.belongsTo('teacher', {async: true})
 });
