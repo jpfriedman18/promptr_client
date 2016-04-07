0a72618d7fab3872b01104d632a3af3563835733
jpfriedman18 <jpfriedman18@gmail.com>
2016-04-07 11:05:13 -0400

    Fix signout to unload previous users data

--- app/application/route.js
+++ app/application/route.js
@@ -11,6 +11,7 @@ export default Ember.Route.extend({
       .then(() => {
         this.get('flashMessages').warning('You have been signed out.');
       })
+      .then(() => this.store.unloadAll())
       .catch(() => {
         this.get('flashMessages')
         .danger('There was a problem. Are you sure you\'re signed-in?');
