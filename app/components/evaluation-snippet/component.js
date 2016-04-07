40eab6e8fe3515ee213516e91611eac80a294b0d
jpfriedman18 <jpfriedman18@gmail.com>
2016-04-06 20:43:38 -0400

    Display teachers evaluations with optional full response

--- app/components/evaluation-snippet/component.js
+++ app/components/evaluation-snippet/component.js
@@ -3,4 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   auth: Ember.inject.service(),
   isTeacher: Ember.computed.alias('auth.isTeacher'),
+  showResponseText: false,
+  actions: {
+    toggleShowResponseText: function(){
+      this.toggleProperty('showResponseText');
+    }
+  }
 });
