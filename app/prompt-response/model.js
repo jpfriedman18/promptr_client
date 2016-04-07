259d5037157cc4669f1f37347767547e2d5467c7
jpfriedman18 <jpfriedman18@gmail.com>
2016-04-07 09:56:25 -0400

    Add student model, show students name on responses

--- app/prompt-response/model.js
+++ app/prompt-response/model.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   text: DS.attr('string'),
   prompt: DS.belongsTo('prompt', {async: true}),
-  evaluation: DS.belongsTo('evaluation', {async: true})
+  evaluation: DS.belongsTo('evaluation', {async: true}),
+  student: DS.belongsTo('student', {async: true})
 });
