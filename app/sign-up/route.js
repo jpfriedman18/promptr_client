9e5a7399b07dfe518673698f0e7e5e4d917dd603
jpfriedman18 <jpfriedman18@gmail.com>
2016-04-07 20:38:12 -0400

    Transition to landings on signup

--- app/sign-up/route.js
+++ app/sign-up/route.js
@@ -8,7 +8,14 @@ export default Ember.Route.extend({
     signUp (credentials) {
       this.get('auth').signUp(credentials)
       .then(() => this.get('auth').signIn(credentials))
-      .then(() => this.transitionTo('application'))
+      .then(() => {
+        if (this.get('auth').get('credentials.profileable_type') === "Teacher"){
+          this.transitionTo('teacher-landing');
+        }
+        else{
+          this.transitionTo('student-landing');
+        }
+      })
       .then(() => {
         this.get('flashMessages')
         .success('Successfully signed-up! You have also been signed-in.');
